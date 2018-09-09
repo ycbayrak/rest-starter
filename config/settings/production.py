25eb75d5f6bf21a509057b15beef3f1633c643a7
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2018-09-09 04:36:26 +0300

    production settings

---
+++ config/settings/production.py
@@ -0,0 +1,26 @@
+from config.settings.base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INSTALLED_APPS.append('compress')
+COMPRESS_ENABLED = True
+COMPRESS_OUTPUT_DIR = 'bundles'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
+)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
