0c13f7b1da49ecefadc7cbd3444963fa37d2d2d3
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2018-09-09 04:36:45 +0300

    import path for django 2.0

--- v1/accounts/admin.py
+++ v1/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models.profile import Profile
-from .models.user import User
+from v1.accounts.models.profile import Profile
+from v1.accounts.models.user import User
 
 
 admin.site.register(Profile)
--- v1/accounts/models/profile.py
+++ v1/accounts/models/profile.py
@@ -1,9 +1,10 @@
-from v1.accounts.models.user import User
 from django.db import models
+
+from v1.accounts.models.user import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(blank=True)
 
     def __str__(self):
