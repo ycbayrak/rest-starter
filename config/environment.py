627c92d1f02e86d4c0aee245d6a5b1ff1f19457e
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2018-09-09 04:36:08 +0300

    environment update.

--- config/environment.py
+++ config/environment.py
@@ -3,3 +3,6 @@ SETTINGS_MODULE = 'config.settings.local'
 
 if ENVIRONMENT == 'local':
     SETTINGS_MODULE = 'config.settings.local'
+
+if ENVIRONMENT == 'prod':
+    SETTINGS_MODULE = 'config.settings.production'
